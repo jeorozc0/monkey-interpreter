20df9474ce8c9fbb47e6a083d08bff7e69879bf4
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-16 13:20:40 -0400

    create function to parse return statement
    update switch statement and create a parser function for our return statement

--- parser/parser.go
+++ parser/parser.go
@@ -59,6 +59,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -76,6 +78,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	//TODO: We're skipping expressions until we ecounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
