d795e3f1bc2ddb90dc32204c82751ab2bfdb7982
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-20 12:16:14 -0400

    expanded repl to print ast nodes

--- repl/repl.go
+++ repl/repl.go
@@ -6,27 +6,36 @@ import (
 	"io"
 
 	"jeorozco.com/go/monkey_interpreter/lexer"
-	"jeorozco.com/go/monkey_interpreter/token"
+	"jeorozco.com/go/monkey_interpreter/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
-
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
