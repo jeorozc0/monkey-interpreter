b2e3e8e066a6ef57fb735c0ed65842a5fe62301c
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-20 20:27:38 -0400

    add evaluation to repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"jeorozco.com/go/monkey_interpreter/evaluator"
 	"jeorozco.com/go/monkey_interpreter/lexer"
 	"jeorozco.com/go/monkey_interpreter/parser"
 )
@@ -27,8 +28,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
