2a38cbe126ceca82df9c7de862545f3cc5a41139
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-22 14:55:53 -0400

    create environment instance for repl

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"jeorozco.com/go/monkey_interpreter/evaluator"
 	"jeorozco.com/go/monkey_interpreter/lexer"
+	"jeorozco.com/go/monkey_interpreter/object"
 	"jeorozco.com/go/monkey_interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -28,7 +30,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
