13ac4b75ab1e29a5111aabde2fe2db77cef6f617
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-23 21:55:14 -0400

    add test for strings and array methods

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -22,7 +22,11 @@ return false;
 }
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
+[1, 2];
 `
+
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -99,6 +103,14 @@ return false;
 		{token.INT, "10"},
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
