0980d5d888ea17d60fbd758e20db5e33f50395cf
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-23 21:56:09 -0400

    create ast definitions for strings

--- ast/ast.go
+++ ast/ast.go
@@ -125,6 +125,15 @@ type IntegerLiteral struct {
 	Value int64
 }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode()      {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
+
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
