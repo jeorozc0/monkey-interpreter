08d393a15975df1e30c16db83430e4fd97ffb00b
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-07-25 16:54:43 -0400

    add test for hash map evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -217,6 +217,10 @@ return 1;
 			`"Hello" - "World`,
 			"unknown operator: STRING - STRING",
 		},
+		{
+			`{"name": "Monkey"}[fn(x) { x }];`,
+			"unusable as hash key: FUNCTION",
+		},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -373,66 +377,73 @@ func TestBuiltinFunctions(t *testing.T) {
 	}
 }
 
-func TestArrayLiterals(t *testing.T) {
-	input := "[1, 2 * 2, 3 + 3]"
+func TestHashLiterals(t *testing.T) {
+	input := `let two = "two";
+{
+"one": 10 - 9,
+two: 1 + 1,
+"thr" + "ee": 6 / 2,
+4: 4,
+true: 5,
+false: 6
+}`
 	evaluated := testEval(input)
-	result, ok := evaluated.(*object.Array)
+	result, ok := evaluated.(*object.Hash)
 	if !ok {
-		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
+		t.Fatalf("Eval didn't return Hash. got=%T (%+v)", evaluated, evaluated)
 	}
-	if len(result.Elements) != 3 {
-		t.Fatalf("array has wrong num of elements. got=%d",
-			len(result.Elements))
+	expected := map[object.HashKey]int64{
+		(&object.String{Value: "one"}).HashKey():   1,
+		(&object.String{Value: "two"}).HashKey():   2,
+		(&object.String{Value: "three"}).HashKey(): 3,
+		(&object.Integer{Value: 4}).HashKey():      4,
+		TRUE.HashKey():                             5,
+		FALSE.HashKey():                            6,
 	}
-	testIntegerObject(t, result.Elements[0], 1)
-	testIntegerObject(t, result.Elements[1], 4)
-	testIntegerObject(t, result.Elements[2], 6)
+	if len(result.Pairs) != len(expected) {
+		t.Fatalf("Hash has wrong num of pairs. got=%d", len(result.Pairs))
+	}
+	for expectedKey, expectedValue := range expected {
+		pair, ok := result.Pairs[expectedKey]
+		if !ok {
+			t.Errorf("no pair for given key in Pairs")
+		}
+		testIntegerObject(t, pair.Value, expectedValue)
+	}
 }
 
-func TestArrayIndexExpressions(t *testing.T) {
+func TestHashIndexExpressions(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected interface{}
 	}{
 		{
-			"[1, 2, 3][0]",
-			1,
+			`{"foo": 5}["foo"]`,
+			5,
 		},
 		{
-			"[1, 2, 3][1]",
-			2,
-		},
-		{
-			"[1, 2, 3][2]",
-			3,
-		},
-		{
-			"let i = 0; [1][i];",
-			1,
-		},
-		{
-			"[1, 2, 3][1 + 1];",
-			3,
-		},
-		{
-			"let myArray = [1, 2, 3]; myArray[2];",
-			3,
-		},
-		{
-			"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];",
-			6,
-		},
-		{
-			"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]",
-			2,
-		},
-		{
-			"[1, 2, 3][3]",
+			`{"foo": 5}["bar"]`,
 			nil,
 		},
 		{
-			"[1, 2, 3][-1]",
+			`let key = "foo"; {"foo": 5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
 			nil,
+		},
+		{
+			`{5: 5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
 		},
 	}
 	for _, tt := range tests {
